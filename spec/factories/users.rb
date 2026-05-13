52780ad911dadbdb48b8ca87bc75d960f70c058c
yusankawayu-stack <yusan.kawayu@gmail.com>
2026-05-13 11:25:12 +0900

    各モデルのテスト

--- spec/factories/users.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    name {Faker::Name.last_name}
-    email {Faker::Internet.email}
-    password {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-  end
-end
